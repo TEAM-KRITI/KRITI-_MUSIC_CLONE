eed003130fa77b9ebefef8d1f0a6777fd9bcc19c
TEAM KRITI BOTS <rejemtahir@gmail.com>
2025-04-14 22:33:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Badnam019/sudheer-",
+    "https://github.com/TEAM-KRITI/KRITI-_MUSIC_CLONE",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
